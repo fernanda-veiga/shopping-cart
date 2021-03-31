8b3d4a8d6c01195e493b0d207eda957ebb26567e
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-03-31 14:53:13 -0300

    Added the the App structure and imported all needed components.

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,23 @@
 import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Header from "./Header";
+import Homepage from "./Homepage";
+import Shop from "./Shop";
+import Footer from "./Footer";
 import "../styles/App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route exact path="/shop" component={Shop} />
+        </Switch>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
