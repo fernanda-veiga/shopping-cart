afa8bd78224b768d2e25fbdf20c3963116180acc
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-03-31 14:55:43 -0300

    Added links to the Homepage and the Shop page on the Header.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
@@ -6,6 +7,16 @@ function Header() {
       <div className="logo">
         <h1>Bouquet Shop</h1>
       </div>
+      <nav>
+        <ul>
+          <Link to="/">
+            <li>Home</li>
+          </Link>
+          <Link to="/shop">
+            <li>Shop</li>
+          </Link>
+        </ul>
+      </nav>
     </div>
   );
 }
