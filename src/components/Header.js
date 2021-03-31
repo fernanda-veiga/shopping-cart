2d9733edcc83226dad4a099a1bc5f72e1ce966b1
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-03-31 16:31:56 -0300

    Fixed teh styling of the Header component. Added a cart icon that will show the number of items in the cart.

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,20 +4,36 @@ import "../styles/Header.css";
 
 function Header() {
   return (
-    <header className="navbar">
-      <div className="logo">
-        <h1>Bouquet Shop</h1>
+    <header>
+      <div className="navbar">
+        <div className="logo">
+          <h1>Bouquet Shop</h1>
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <Link className="link" to="/">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link className="link" to="/shop">
+                Shop
+              </Link>
+            </li>
+            <li>
+              <div className="cart">
+                <div className="cart-icon">
+                  <i class="fas fa-shopping-bag"></i>
+                </div>
+                <div className="cart-number">
+                  <p>0</p>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </nav>
       </div>
-      <nav>
-        <ul>
-          <Link className="link" to="/">
-            <li>Home</li>
-          </Link>
-          <Link className="link" to="/shop">
-            <li>Shop</li>
-          </Link>
-        </ul>
-      </nav>
     </header>
   );
 }
