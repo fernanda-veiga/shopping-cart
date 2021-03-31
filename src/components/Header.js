a52ff9f1643310c8e832d0582d2cbe5ff2cfdbef
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-03-31 14:52:25 -0300

    initial file setup.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 function Header() {
-  return <header></header>;
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <h1>Bouquet Shop</h1>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
