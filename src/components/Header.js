6daf99a352bc7ea40c68ee4c4a3a075c6ad94f11
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-03-31 15:11:42 -0300

    Added styling to the Header component.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../styles/Header.css";
 
 function Header() {
+  const linkStyle = { textDecoration: "none", color: "#161854" };
+
   return (
-    <div className="navbar">
-      <div className="logo">
-        <h1>Bouquet Shop</h1>
+    <header>
+      <div className="navbar">
+        <div className="logo">
+          <h1>Bouquet Shop</h1>
+        </div>
+        <nav>
+          <ul>
+            <Link style={linkStyle} to="/">
+              <li>Home</li>
+            </Link>
+            <Link style={linkStyle} to="/shop">
+              <li>Shop</li>
+            </Link>
+          </ul>
+        </nav>
       </div>
-      <nav>
-        <ul>
-          <Link to="/">
-            <li>Home</li>
-          </Link>
-          <Link to="/shop">
-            <li>Shop</li>
-          </Link>
-        </ul>
-      </nav>
-    </div>
+    </header>
   );
 }
 
