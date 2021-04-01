844581e214ac71aed29680e431c32867ed8f1a3d
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-03-31 22:18:11 -0300

    Added a Header and a Router that changes between the Homepage and the Shop components. Added the necessary functions to handle these items and passed as props to the components. Fixed the styling of the App component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Header from "./Header";
 import Homepage from "./Homepage";
 import Shop from "./Shop";
-import Footer from "./Footer";
 import "../styles/App.css";
 
 function App() {
+  const [allCount, setAllCount] = useState([
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+  ]);
+  const [cartQuantity, setCartQuantity] = useState(0);
+
+  function handleChange(count, index) {
+    const newAllCount = allCount;
+    newAllCount[index] = count;
+    setAllCount(newAllCount);
+  }
+
+  function addToCart() {
+    const sum = allCount.reduce((a, b) => a + b, 0);
+    setCartQuantity(sum);
+  }
+
   return (
     <BrowserRouter>
       <div className="App">
-        <Header />
+        <Header cartQuantity={cartQuantity} />
         <Switch>
           <Route exact path="/" component={Homepage} />
-          <Route exact path="/shop" component={Shop} />
+          <Route
+            exact
+            path="/shop"
+            render={(props) => (
+              <Shop
+                {...props}
+                handleChange={handleChange}
+                addToCart={addToCart}
+              />
+            )}
+          />
         </Switch>
-        <Footer />
       </div>
     </BrowserRouter>
   );
