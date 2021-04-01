536267e6e123f6d80edad3f4eed2482f6dd23178
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-03-31 22:14:52 -0300

    Added a file to create an object that stores all the product images.

---
+++ src/bouquets.js
@@ -0,0 +1,29 @@
+import bouquet0 from "./images/bouquet-0.jpg";
+import bouquet1 from "./images/bouquet-1.jpg";
+import bouquet2 from "./images/bouquet-2.jpg";
+import bouquet3 from "./images/bouquet-3.jpg";
+import bouquet4 from "./images/bouquet-4.jpg";
+import bouquet5 from "./images/bouquet-5.jpg";
+import bouquet6 from "./images/bouquet-6.jpg";
+import bouquet7 from "./images/bouquet-7.jpg";
+import bouquet8 from "./images/bouquet-8.jpg";
+import bouquet9 from "./images/bouquet-9.jpg";
+import bouquet10 from "./images/bouquet-10.jpg";
+import bouquet11 from "./images/bouquet-11.jpg";
+
+const bouquets = [
+  { name: "Bouquet #01", img: bouquet0, price: "$35", id: "bouquet0" },
+  { name: "Bouquet #02", img: bouquet1, price: "$45", id: "bouquet1" },
+  { name: "Bouquet #03", img: bouquet2, price: "$40", id: "bouquet2" },
+  { name: "Bouquet #04", img: bouquet3, price: "$50", id: "bouquet3" },
+  { name: "Bouquet #05", img: bouquet4, price: "$35", id: "bouquet4" },
+  { name: "Bouquet #06", img: bouquet5, price: "$40", id: "bouquet5" },
+  { name: "Bouquet #07", img: bouquet6, price: "$25", id: "bouquet6" },
+  { name: "Bouquet #08", img: bouquet7, price: "$30", id: "bouquet7" },
+  { name: "Bouquet #09", img: bouquet8, price: "$50", id: "bouquet8" },
+  { name: "Bouquet #10", img: bouquet9, price: "$45", id: "bouquet9" },
+  { name: "Bouquet #11", img: bouquet10, price: "$30", id: "bouquet10" },
+  { name: "Bouquet #12", img: bouquet11, price: "$25", id: "bouquet11" },
+];
+
+export default bouquets;
